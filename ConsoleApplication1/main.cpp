cf4cb1e5ccf2681bc87e9323f8df77cb71895480
saslfras <nicolas.sosa@stonybrook.edu>
2014-09-25 16:12:01 +0100

    Lab1 Started

---
+++ ConsoleApplication1/main.cpp
@@ -0,0 +1,19 @@
+//Header Files
+#include <iostream>
+
+
+//Global variables go here
+
+//Global functions
+void InitWindow(int width, int height, bool fullscreen){
+
+}
+
+void CleanUp(){
+
+}
+
+//Main Method - Entry Point
+int main(int argc, char*arg[]){
+	return 0;
+}
