3e8239c7b59e2ad5fec38121ef16ef052003f93e
saslfras <nicolas.sosa@stonybrook.edu>
2014-09-25 16:30:21 +0100

    Made it to step 5.3

--- ConsoleApplication1/main.cpp
+++ ConsoleApplication1/main.cpp
@@ -1,8 +1,16 @@
 //Header Files
 #include <iostream>
+//header for SDL2 functionality
+#include <SDL.h>
 
 
 //Global variables go here
+//Pointer to our SDL Windows
+SDL_Window*window;
+
+//Constants to control window cration
+const int WINDOW_WIDTH = 640;
+const int WINDOW_HEIGHT = 480;
 
 //Global functions
 void InitWindow(int width, int height, bool fullscreen){
