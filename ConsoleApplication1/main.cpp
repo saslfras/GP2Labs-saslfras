569ef7358e600dada3cb61dd34fe52ff4a818272
saslfras <nicolas.sosa@stonybrook.edu>
2014-10-02 14:17:13 +0100

    Finished final code, must edit file calls in step 7

--- ConsoleApplication1/main.cpp
+++ ConsoleApplication1/main.cpp
@@ -2,11 +2,15 @@
 #include <iostream>
 //header for SDL2 functionality
 #include <SDL.h>
+#include <SDL_opengl.h>
+#include <gl\GLU.h>
 
 
 //Global variables go here
 //Pointer to our SDL Windows
 SDL_Window*window;
+//SDL GL Context
+SDL_GLContext glcontext = NULL;
 
 //Constants to control window cration
 const int WINDOW_WIDTH = 640;
@@ -30,8 +34,104 @@ void InitWindow(int width, int height, bool fullscreen){
 
 //Used to cleanup once we exit
 void CleanUp(){
+	SDL_GL_DeleteContext(glcontext);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
+}
+
+//Function to initialise OpenGL
+void initOpenGL(){
+	//Create OpenGL Context
+	glcontext = SDL_GL_CreateContext(window);
+	
+	//Something went wrong in creating the context, if it is still NULL
+	if(!glcontext){
+		std::cout<<"Error Creating OpenGL Context"<<SDL_GetError()<<
+		std::endl;
+	}
+	
+	//Smooth shading
+	glShadeModel(GL_SMOOTH);
+	
+	//clear the background to black
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	
+	//Clear the depth buffer to 1.0
+	glClearDepth(1.0f);
+	
+	//Enable depth testing
+	glEnable(GL_DEPTH_TEST);
+	
+	//The depth test to use
+	glDepthFunc(GL_LEQUAL);
+	
+	//Turn on best perspective correction
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+	
+	
+	
+}
+
+//Function to st/reset viewport
+void setViewport(int width, int height){
+	//screen ratio
+	GLfloat ratio;
+	
+	//make sure height is always above 0
+	if(heigt == 0){
+		height = 1;
+	}
+	
+	//calculate screen ratio
+	ratio = (GLfloat)width/(GLfloat)height;
+	
+	//Setup viewport
+	glViewport(0,0,(GLsizei)width, (GLsizei)height);
+	
+	//Change to project matrix mode
+	glMatrixMode(GL_PROJECTIONC);
+	glLoadIdentity();
+	
+	//Calculate perspetive matrix, using glu library functions
+	gluPerspective(45.0f,ratio,0.1f,100.0f);
+	
+	//Switch to ModelView
+	glMatrixMode(GL_MODELVIEW);
+	
+	//Reset using the identity matrix
+	glLoadIdentity();
+	
+}
+
+//Function to draw
+void render(){
+	//Set the clear color(background
+	glClearColor(0.0f,0.0f,0.0f,0.0f);
+	
+	//clear the color and depth buffer
+	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+	
+	//Switch to ModelView
+	glMatrixMode(GL_MODELVIEW);
+	//Reset using the identitiy matrix
+	glLoadIdentity();
+	//Translate to -5.0f on z-axis
+	glTranslatef(0.0f,0.0f,-5.0f);
+	//Begin drawing triangles
+	glBegin(GL_TRIANGLES);
+		glColor3f(1.0f,0.0f,0.0f);//Color of the vertice
+		glVertex3f(0.0f,1.0f,0.0f);//Top
+		glVertex3f(-1.0f,-1.0f,0.0f);//Bottom Left
+		glVertex3f(1.0f,-1.0f,0.0f);//BottomRight
+	glEnd();
+	
+	//require to swap the back and front buffer
+	SDL_GL_SwapWindow(window);
+}
+
+//Function to update game state
+voide update(){
+	
 }
 
 //Main Method - Entry Point
@@ -43,6 +143,11 @@ int main(int argc, char*arg[]){
 	}
 
 	InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, false);
+	
+	//Call our InitOpenGL Function
+	initOpenGL();
+	//Set our viewport
+	setViewport(WINDOW_WIDTH,WINDOW_HEIGHT);
 
 	SDL_Event event;
 	while (running){
@@ -53,6 +158,9 @@ int main(int argc, char*arg[]){
 				running = false;
 			}
 		}
+		
+		update();
+		render();
 
 	}
 
